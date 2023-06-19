d4e3e561b7252624332c0108b4c59f3149648cd3
Estherifeoma1 <esthernnamani111@gmail.com>
2023-06-19 13:44:05 -0700

    Software is eating the World

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -3,32 +3,27 @@
 #include <time.h>
 /**
  * main - Entry point
- * Decription: prints all possible combinationof a pair of two digit numberswithout any repetition
- * Return: 0 on success
+ * Return: Always 0
  */
 
 int main(void)
 {
-	int i, j;
+	int p, q;
 
-	for (i = 0; i <= 99; i++)
+	for (p = 0; p <= 98; p++)
 	{
-		for (j = 0; j <= 99; j++)
-		{
-			if (i < j && i != j)
-			{
-				putchar((i / 10) + '0');
-				putchar((i % 10) + '0');
-				putchar(',');
-				putchar((j / 10) + '0');
-				putchar((j % 10) + '0');
-				if (i != 98 || j != 99)
-				{
-					putchar(',');
-					putchar(' ');
-				}
-			}
-		}
+	for (q = p + 1; q <= 99; q++)
+	{
+	putchar((p / 10) + '0');
+	putchar((p % 10) + '0');
+	putchar(' ');
+	putchar((q / 10) + '0');
+	putchar((q % 10) + '0');
+	if (p == 98 && q == 99)
+	continue;
+	putchar(',');
+	putchar(' ');
+	}
 	}
 	putchar('\n');
 	return (0);
