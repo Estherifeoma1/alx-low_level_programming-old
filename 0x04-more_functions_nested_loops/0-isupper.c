7aa95b4faa2d45aeeee1535e5adbaee2e3f996da
Estherifeoma1 <esthernnamani111@gmail.com>
2023-06-22 13:27:06 -0700

    main.h

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isupper - checks for lowercase character
+ * @c: The characterto be checked
+ * Return: 1 for uppercase character or 0 for anything else
+ */
+
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= '2')
+		return (1);
+	else
+		return (0);
+}
