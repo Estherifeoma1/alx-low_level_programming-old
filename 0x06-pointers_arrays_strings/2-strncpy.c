52b58597fda6c1fd312d224006d0d67d31b8933a
Estherifeoma1 <esthernnamani111@gmail.com>
2023-06-29 15:51:35 -0700

    strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -11,13 +11,19 @@
 
 char *_strncpy(char *dest, char *src, int n)
 {
-int count_of_bytes;
+	int j;
 
-for (count_of_bytes = 0; count_of_bytes < n && src[count_of_bytes] != '\0'; count_of_bytes++)
-dest[count_of_bytes] = src[count_of_bytes];
+	j = 0;
+	while (j < n && src[j] != '\0')
+	{
+		dest[j] = src[j];
+		j++;
+	}
+	while (j < n)
+	{
+		dest[j] = '\0';
+		j++;
+	}
 
-for (; count_of_bytes < n; count_of_bytes++)
-dest[count_of_bytes] = '\0';
-
-return (dest);
+	return (dest);
 }
