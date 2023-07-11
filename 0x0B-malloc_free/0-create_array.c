00edf4729a1e3dc4b69296156717da5d725ff3d7
Estherifeoma1 <esthernnamani111@gmail.com>
2023-07-11 10:47:00 -0700

    0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -7,6 +7,7 @@
  * @c: char to assign
  * Description: create array of size size and assign char c
  * Return: pointer to array, NULL if fail
+ *
  */
 
 char *create_array(unsigned int size, char c)
