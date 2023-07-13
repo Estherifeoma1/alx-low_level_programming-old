46d0757fe119b816f7fe104d16d4f72db743b2ad
Estherifeoma1 <esthernnamani111@gmail.com>
2023-07-13 16:04:23 -0700

    101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -21,7 +21,7 @@ int is_digit(char *s)
 			return (0);
 		i++;
 	}
-	return (i);
+	return (1);
 }
 
 /**
@@ -47,7 +47,7 @@ int _strlen(char *s)
  */
 void errors(void)
 {
-	printf("Errors\n");
+	printf("Error\n");
 	exit(98);
 }
 
@@ -64,7 +64,7 @@ int main(int argc, char *argv[])
 	char *s1, *s2;
 	int len1, len2, len, i, carry, digit1, digit2, *result, a = 0;
 
-	s1 = argv[i], s2 = argv[2];
+	s1 = argv[1], s2 = argv[2];
 	if (argc != 3 || !is_digit(s1) || !is_digit(s2))
 		error();
 	len1 = _strlen(s1);
